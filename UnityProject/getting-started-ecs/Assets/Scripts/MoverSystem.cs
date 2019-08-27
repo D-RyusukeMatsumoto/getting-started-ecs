62911b9f5bf5a529c6162b5c6430106ade4a5b26
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2019-08-27 17:20:01 +0900

    Complete Getting Started with ECS in Unity2019
    https://www.youtube.com/watch?v=ILfUuBLfzGI&list=PLzDRvYVwl53s40yP5RQXitbT--IRcHqba
    ↑この動画の内容までが完了した

---
+++ UnityProject/getting-started-ecs/Assets/Scripts/MoverSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+
+public class MoverSystem : ComponentSystem
+{
+    
+    
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, ref MoveSpeedComponent moveSpeedComponent) =>
+        {
+            translation.Value.y += moveSpeedComponent.moveSpeed * Time.deltaTime;
+            if (5f < translation.Value.y)
+                moveSpeedComponent.moveSpeed = -math.abs(moveSpeedComponent.moveSpeed);
+
+            if (translation.Value.y < -5f)
+                moveSpeedComponent.moveSpeed = +math.abs(moveSpeedComponent.moveSpeed);
+
+        });    
+    }
+}
--- UnityProject/getting-started-ecs/Assets/Scripts/Testing.cs
+++ UnityProject/getting-started-ecs/Assets/Scripts/Testing.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Collections;
+using Unity.Mathematics;
 using Unity.Rendering;
 
 
@@ -22,17 +23,34 @@ public class Testing : MonoBehaviour
             typeof(LevelComponent),
             typeof(Translation),
             typeof(RenderMesh),
-            typeof(LocalToWorld));
+            typeof(LocalToWorld),
+            typeof(MoveSpeedComponent));
 
-        NativeArray<Entity> entityArray = new NativeArray<Entity>(2000, Allocator.Temp);
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(10000, Allocator.Temp);
         entityManager.CreateEntity(entityArchetype, entityArray);
         for (var i = 0; i < entityArray.Length; ++i)
         {
             Entity entity = entityArray[i];
-            entityManager.SetComponentData(entity, new LevelComponent{level = 10});
+            entityManager.SetComponentData(entity, 
+                new LevelComponent
+                {
+                    level = 10
+                });
+            entityManager.SetComponentData(entity, 
+                new MoveSpeedComponent
+                {
+                    moveSpeed = UnityEngine.Random.Range(1f, 2f)
+                });
+            entityManager.SetComponentData(entity, 
+                new Translation
+                {
+                    Value = new float3(UnityEngine.Random.Range(-8f, 8f),UnityEngine.Random.Range(-5f, 5f), 0)
+                });
             
             entityManager.SetSharedComponentData(entity, new RenderMesh
             {
+                mesh = mesh,
+                material = material,
             });
         }
         entityArray.Dispose();
