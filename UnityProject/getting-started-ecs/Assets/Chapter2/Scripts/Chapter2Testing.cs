999a5db6be4c110e368f479db691c212bbfa9cf7
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2019-08-30 16:02:18 +0900

    Update Chapter2

--- UnityProject/getting-started-ecs/Assets/Chapter2/Scripts/Chapter2Testing.cs
+++ UnityProject/getting-started-ecs/Assets/Chapter2/Scripts/Chapter2Testing.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Unity.Mathematics;
 using Unity.Collections;
 using Unity.Burst;
-using UnityEditor.Profiling.Memory.Experimental;
+using UnityEngine.Jobs;
 
 
 namespace Chapter2
@@ -51,33 +51,44 @@ namespace Chapter2
 
             if (useJobs)
             {
-                NativeArray<float3> positionArray = new NativeArray<float3>(ieinuList.Count, Allocator.TempJob);
+                //NativeArray<float3> positionArray = new NativeArray<float3>(ieinuList.Count, Allocator.TempJob);
                 NativeArray<float> moveYArray = new NativeArray<float>(ieinuList.Count, Allocator.TempJob);
-
+                TransformAccessArray transformAccessArray = new TransformAccessArray(ieinuList.Count);
+                
                 for (var i = 0; i < ieinuList.Count; ++i)
                 {
-                    positionArray[i] = ieinuList[i].transform.position;
+                    //positionArray[i] = ieinuList[i].transform.position;
                     moveYArray[i] = ieinuList[i].moveY;
+                    transformAccessArray.Add(ieinuList[i].transform);
                 }
 
+/*
                 ReallyToughParallelJob reallyToughParallelJob = new ReallyToughParallelJob
                 {
                     deltaTime = Time.deltaTime,
                     positionArray = positionArray,
                     moveYArray = moveYArray,
                 };
+*/
 
-                JobHandle jobHandle = reallyToughParallelJob.Schedule(ieinuList.Count, 100);
-                jobHandle.Complete();
+                ReallyToughParallelJobTransforms reallyToughParallelJobTransforms = new ReallyToughParallelJobTransforms
+                {
+                    deltaTime = Time.deltaTime,
+                    moveYArray = moveYArray,
+                };
+                JobHandle jobHandle = reallyToughParallelJobTransforms.Schedule(transformAccessArray);
+                jobHandle.Complete();                
+
 
                 for (var i = 0; i < ieinuList.Count; ++i)
                 {
-                    ieinuList[i].transform.position = positionArray[i];
+                    //ieinuList[i].transform.position = positionArray[i];
                     ieinuList[i].moveY = moveYArray[i];
                 }
                 
-                positionArray.Dispose();
+                //positionArray.Dispose();
                 moveYArray.Dispose();
+                transformAccessArray.Dispose();
 
             }
             else
@@ -121,7 +132,7 @@ namespace Chapter2
                 }
             }
 */
-            //Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
+            Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
         }
 
 
@@ -192,6 +203,40 @@ namespace Chapter2
             }
 
         }
+    }
+
+
+    [BurstCompile]
+    public struct ReallyToughParallelJobTransforms : IJobParallelForTransform
+    {
+
+        public NativeArray<float> moveYArray;
+        [ReadOnly] public float deltaTime;
+        
+        public void Execute(
+            int index,
+            TransformAccess transform)
+        {
+            
+            transform.position += new Vector3(0, moveYArray[index] * deltaTime, 0f);
+            if (5f < transform.position.y)
+            {
+                moveYArray[index] = -math.abs(moveYArray[index]);
+            }
+            else if (transform.position.y < -5f)
+            {
+                moveYArray[index] = +math.abs(moveYArray[index]);
+            }
+            
+            float value = 0f;
+            for (var i = 0; i < 1000; ++i)
+            {
+                value = math.exp10(math.sqrt(value));
+            }
+
+        }
+        
+        
     }
     
     
