0ba379e5d35ed42156d58fec0a82532a5bff321f
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2019-09-13 18:55:32 +0900

    Update Chapter2 scripts
    混乱を避けるため利用していないコードは削除した

--- UnityProject/getting-started-ecs/Assets/Chapter2/Scripts/Chapter2Testing.cs
+++ UnityProject/getting-started-ecs/Assets/Chapter2/Scripts/Chapter2Testing.cs
@@ -26,8 +26,7 @@ namespace Chapter2
             public float moveY;
         }
 
-
-
+        
         void Start()
         {
             ieinuList = new List<Ieinu>();
@@ -46,32 +45,16 @@ namespace Chapter2
         
         void Update()
         {
-            float startTime = Time.realtimeSinceStartup;
-
-
             if (useJobs)
             {
-                //NativeArray<float3> positionArray = new NativeArray<float3>(ieinuList.Count, Allocator.TempJob);
                 NativeArray<float> moveYArray = new NativeArray<float>(ieinuList.Count, Allocator.TempJob);
                 TransformAccessArray transformAccessArray = new TransformAccessArray(ieinuList.Count);
                 
                 for (var i = 0; i < ieinuList.Count; ++i)
                 {
-                    //positionArray[i] = ieinuList[i].transform.position;
                     moveYArray[i] = ieinuList[i].moveY;
                     transformAccessArray.Add(ieinuList[i].transform);
                 }
-
-/*
-                ReallyToughParallelJob reallyToughParallelJob = new ReallyToughParallelJob
-                {
-                    deltaTime = Time.deltaTime,
-                    positionArray = positionArray,
-                    moveYArray = moveYArray,
-                };
-                JobHandle jobHandle = reallyToughParallelJob.Schedule(ieinuList.Count, 100);
-                jobHandle.Complete();
-*/
 
                 ReallyToughParallelJobTransforms reallyToughParallelJobTransforms = new ReallyToughParallelJobTransforms
                 {
@@ -81,17 +64,13 @@ namespace Chapter2
                 JobHandle jobHandle = reallyToughParallelJobTransforms.Schedule(transformAccessArray);
                 jobHandle.Complete();                
 
-
                 for (var i = 0; i < ieinuList.Count; ++i)
                 {
-                    //ieinuList[i].transform.position = positionArray[i];
                     ieinuList[i].moveY = moveYArray[i];
                 }
                 
-                //positionArray.Dispose();
                 moveYArray.Dispose();
                 transformAccessArray.Dispose();
-
             }
             else
             {
@@ -114,91 +93,6 @@ namespace Chapter2
                     }
                 }
             
-            }
-/*
-            if (useJobs)
-            {
-                NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
-                for (var i = 0; i < 10; ++i)
-                {
-                    JobHandle jobHandle = this.ReallyToughTaskJob();
-                    jobHandleList.Add(jobHandle);
-                }
-                JobHandle.CompleteAll(jobHandleList);
-                jobHandleList.Dispose();
-            }
-            else
-            {
-                for (var i = 0; i < 10; ++i)
-                {
-                    this.ReallyToughTask();                
-                }
-            }
-*/
-            //Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
-        }
-
-
-        void ReallyToughTask()
-        {
-            float value = 0f;
-            for (var i = 0; i < 50000; ++i)
-            {
-                value = math.exp10(math.sqrt(value));
-            }
-        }
-
-
-        JobHandle ReallyToughTaskJob()
-        {
-            ReallyToughJob job = new ReallyToughJob();
-             return job.Schedule();
-        }
-        
-        
-    }
-
-
-    [BurstCompile]
-    public struct ReallyToughJob : IJob
-    {
-        public void Execute()
-        {
-            float value = 0f;
-            for (var i = 0; i < 50000; ++i)
-            {
-                value = math.exp10(math.sqrt(value));
-            }
-        }
-    }
-
-
-    [BurstCompile]
-    public struct ReallyToughParallelJob : IJobParallelFor
-    {
-
-        public NativeArray<float3> positionArray;
-        public NativeArray<float> moveYArray;
-        [ReadOnly] public float deltaTime;
-        
-        
-        public void Execute(
-            int index)
-        {
-            positionArray[index] += new float3(0, moveYArray[index] * deltaTime, 0f);
-            if (5f < positionArray[index].y)
-            {
-                moveYArray[index] = -math.abs(moveYArray[index]);
-            }
-            else if (positionArray[index].y < -5f)
-            {
-                moveYArray[index] = +math.abs(moveYArray[index]);
-            }
-            
-            float value = 0f;
-            for (var i = 0; i < 1000; ++i)
-            {
-                value = math.exp10(math.sqrt(value));
             }
         }
     }
